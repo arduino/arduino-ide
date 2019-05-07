e14ac4a396628644c28f404e39007846cb3ff62d
Akos Kitta <kittaakos@typefox.io>
2019-05-07 10:19:32 +0200

    [ui] Activate the `Output` view.
     - When new output arrives from the daemon.
     - Make sure, the current output channel is selected
    before appending any message to it.
    
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- arduino-ide-extension/src/browser/tool-output/client-service-impl.ts
+++ arduino-ide-extension/src/browser/tool-output/client-service-impl.ts
@@ -1,6 +1,7 @@
 import { ToolOutputServiceClient } from "../../common/protocol/tool-output-service";
 import { injectable, inject } from "inversify";
 import { OutputChannelManager } from "@theia/output/lib/common/output-channel";
+import { OutputContribution } from "@theia/output/lib/browser/output-contribution";
 
 @injectable()
 export class ToolOutputServiceClientImpl implements ToolOutputServiceClient {
@@ -8,9 +9,15 @@ export class ToolOutputServiceClientImpl implements ToolOutputServiceClient {
     @inject(OutputChannelManager)
     protected readonly outputChannelManager: OutputChannelManager;
 
+    @inject(OutputContribution)
+    protected readonly outputContribution: OutputContribution;
+
     onNewOutput(tool: string, chunk: string): void {
-        const channel = this.outputChannelManager.getChannel(`Arduino: ${tool}`);
-        channel.append(chunk);
+        this.outputContribution.openView({ reveal: true }).then(() => {
+            const channel = this.outputChannelManager.getChannel(`Arduino: ${tool}`);
+            channel.setVisibility(true);
+            channel.append(chunk);
+        })
     }
 
 }
