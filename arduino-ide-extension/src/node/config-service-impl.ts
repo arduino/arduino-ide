05850b5f2728b87d6c362277cf4cb1d2ba38b2f4
Akos Kitta <kittaakos@typefox.io>
2019-10-25 15:07:14 +0200

    Ensure default data and sketch dir existence.
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- arduino-ide-extension/src/node/config-service-impl.ts
+++ arduino-ide-extension/src/node/config-service-impl.ts
@@ -1,5 +1,8 @@
-import { injectable, inject } from 'inversify';
+import { mkdirpSync, existsSync } from 'fs-extra';
+import { injectable, inject, postConstruct } from 'inversify';
 import URI from '@theia/core/lib/common/uri';
+import { FileUri } from '@theia/core/lib/node/file-uri';
+import { Deferred } from '@theia/core/lib/common/promise-util';
 import { ConfigService, Config } from '../common/protocol/config-service';
 import { ArduinoCli } from './arduino-cli';
 
@@ -8,9 +11,24 @@ export class ConfigServiceImpl implements ConfigService {
 
     @inject(ArduinoCli)
     protected readonly cli: ArduinoCli;
+    protected readonly config: Deferred<Config> = new Deferred();
+
+    @postConstruct()
+    protected init(): void {
+        this.cli.getDefaultConfig().then(config => {
+            const { dataDirUri, sketchDirUri } = config;
+            for (const uri of [dataDirUri, sketchDirUri]) {
+                const path = FileUri.fsPath(uri);
+                if (!existsSync(path)) {
+                    mkdirpSync(path);
+                }
+            }
+            this.config.resolve(config);
+        });
+    }
 
     async getConfiguration(): Promise<Config> {
-        return this.cli.getDefaultConfig();
+        return this.config.promise;
     }
 
     async getVersion(): Promise<string> {
