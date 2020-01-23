2855026cec1d85a47d875cd56c710decbfda9dd2
Miro Spönemann <miro.spoenemann@typefox.io>
2020-01-23 13:42:30 +0100

    [debugger] Don't allow running more than one debug session

--- arduino-debugger-extension/src/browser/frontend-module.ts
+++ arduino-debugger-extension/src/browser/frontend-module.ts
@@ -1,14 +1,17 @@
 import { ContainerModule } from 'inversify';
 import { VariableContribution } from '@theia/variable-resolver/lib/browser';
 import { ArduinoVariableResolver } from './arduino-variable-resolver';
+import { DebugSessionManager } from '@theia/debug/lib/browser/debug-session-manager';
 import { DebugFrontendApplicationContribution } from '@theia/debug/lib/browser/debug-frontend-application-contribution';
 import { DebugConfigurationManager } from '@theia/debug/lib/browser/debug-configuration-manager';
 import { ArduinoDebugConfigurationManager } from './arduino-debug-configuration-manager';
 import { ArduinoDebugFrontendApplicationContribution } from './arduino-debug-frontend-application-contribution';
+import { ArduinoDebugSessionManager } from './arduino-debug-session-manager';
 
 export default new ContainerModule((bind, unbind, isBound, rebind) => {
     bind(ArduinoVariableResolver).toSelf().inSingletonScope();
     bind(VariableContribution).toService(ArduinoVariableResolver);
+    rebind(DebugSessionManager).to(ArduinoDebugSessionManager).inSingletonScope();
     rebind(DebugConfigurationManager).to(ArduinoDebugConfigurationManager).inSingletonScope();
     rebind(DebugFrontendApplicationContribution).to(ArduinoDebugFrontendApplicationContribution);
 });
