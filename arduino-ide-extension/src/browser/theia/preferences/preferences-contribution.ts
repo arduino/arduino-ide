f605994d7dfb9d052b554285f0abdc15dce38272
Akos Kitta <kittaakos@typefox.io>
2020-08-14 11:24:59 +0200

    fixed duplicate preferences menu item.
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- arduino-ide-extension/src/browser/theia/preferences/preferences-contribution.ts
+++ arduino-ide-extension/src/browser/theia/preferences/preferences-contribution.ts
@@ -1,5 +1,4 @@
 import { injectable } from 'inversify';
-import { isOSX } from '@theia/core/lib/common/os';
 import { MenuModelRegistry } from '@theia/core/lib/common/menu';
 import { KeybindingRegistry } from '@theia/core/lib/browser/keybinding';
 import { CommonCommands, CommonMenus } from '@theia/core/lib/browser';
@@ -10,10 +9,9 @@ export class PreferencesContribution extends TheiaPreferencesContribution {
 
     registerMenus(registry: MenuModelRegistry): void {
         super.registerMenus(registry);
-        if (isOSX) {
-            // The settings group: preferences, CLI config is not part of the `File` menu on macOS.
-            registry.unregisterMenuAction(CommonCommands.OPEN_PREFERENCES.id, CommonMenus.FILE_SETTINGS_SUBMENU_OPEN);
-        }
+        // The settings group: preferences, CLI config is not part of the `File` menu on macOS.
+        // On Windows and Linux, we rebind it to `Preferences...`. It is safe to remove here.
+        registry.unregisterMenuAction(CommonCommands.OPEN_PREFERENCES.id, CommonMenus.FILE_SETTINGS_SUBMENU_OPEN);
     }
 
     registerKeybindings(registry: KeybindingRegistry): void {
