e6cbefb880fa7464f3cafaea350e0d1052180196
Francesco Stasi <f.stasi@me.com>
2021-06-22 16:01:46 +0200

    eslint to fix unused imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,4 @@
 module.exports = {
-    ignorePatterns: [
-        'node_modules/*',
-        '/*',
-        '!arduino-ide-extension',
-        'arduino-ide-extension/*',
-        '!arduino-ide-extension/src',
-        'arduino-ide-extension/src/node/cli-protocol',
-    ],
     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
     parserOptions: {
         ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
@@ -15,6 +7,20 @@ module.exports = {
             jsx: true, // Allows for the parsing of JSX
         },
     },
+    ignorePatterns: [
+        'node_modules/*',
+        '**/node_modules/*',
+        '.node_modules/*',
+        '.github/*',
+        '.browser_modules/*',
+        'docs/*',
+        'scripts/*',
+        'electron/*',
+        'electron-app/*',
+        'browser-app/*',
+        'plugins/*',
+        'arduino-ide-extension/src/node/cli-protocol',
+    ],
     settings: {
         react: {
             version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
@@ -27,14 +33,24 @@ module.exports = {
         'plugin:prettier/recommended',
         'prettier', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     ],
-    plugins: ['prettier'],
-    root: true,
+    plugins: ['prettier', 'unused-imports'],
     rules: {
         '@typescript-eslint/no-unused-expressions': 'off',
         '@typescript-eslint/no-namespace': 'off',
         '@typescript-eslint/no-var-requires': 'off',
         '@typescript-eslint/no-empty-function': 'warn',
         '@typescript-eslint/no-empty-interface': 'warn',
+        'no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': [
+            'warn',
+            {
+                vars: 'all',
+                varsIgnorePattern: '^_',
+                args: 'after-used',
+                argsIgnorePattern: '^_',
+            },
+        ],
         'react/display-name': 'warn',
         eqeqeq: ['error', 'smart'],
         'guard-for-in': 'off',
