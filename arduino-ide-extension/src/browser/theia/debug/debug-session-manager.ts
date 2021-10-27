acbb164c3c9b15a05afa7e1b7de00494294d3d8a
Mark Sujew <mark.sujew@typefox.io>
2021-10-27 12:56:17 +0200

    Fix cortex-debug related debugging issue (#578)
    * Fix cortex-debug related debugging issue
    
    * Update arduino-ide-extension/src/browser/theia/debug/debug-session-manager.ts
    
    Co-authored-by: Francesco Stasi <francescomaria.stasi@gmail.com>

--- arduino-ide-extension/src/browser/theia/debug/debug-session-manager.ts
+++ arduino-ide-extension/src/browser/theia/debug/debug-session-manager.ts
@@ -62,4 +62,9 @@ export class DebugSessionManager extends TheiaDebugSessionManager {
       }
     );
   }
+  // TODO: remove as https://github.com/eclipse-theia/theia/issues/10164 is fixed
+  async terminateSessions(): Promise<void> {
+    await super.terminateSessions();
+    this.destroy(this.currentSession?.id);
+  }
 }
