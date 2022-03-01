a5a6a0b611fb7939b7cff73cee819adf73c49914
Alberto Iannaccone <a.iannaccone@arduino.cc>
2022-03-01 08:24:29 +0000

    Go to download page when automatic update fails (#871)
    * add preference to set a custom update url
    
    * go to download page when update fails
    
    * fix i18n check

--- arduino-ide-extension/src/browser/dialogs/ide-updater/ide-updater-component.tsx
+++ arduino-ide-extension/src/browser/dialogs/ide-updater/ide-updater-component.tsx
@@ -1,3 +1,4 @@
+import { WindowService } from '@theia/core/lib/browser/window/window-service';
 import { nls } from '@theia/core/lib/common';
 import { shell } from 'electron';
 import * as React from 'react';
@@ -8,6 +9,7 @@ import ProgressBar from '../../components/ProgressBar';
 
 export type IDEUpdaterComponentProps = {
   updateInfo: UpdateInfo;
+  windowService: WindowService;
   downloadFinished?: boolean;
   downloadStarted?: boolean;
   progress?: ProgressInfo;
@@ -22,6 +24,7 @@ export const IDEUpdaterComponent = ({
   updateInfo: { version, releaseNotes },
   downloadStarted = false,
   downloadFinished = false,
+  windowService,
   progress,
   error,
   onDownload,
@@ -62,98 +65,147 @@ export const IDEUpdaterComponent = ({
     </button>
   );
 
+  const DownloadCompleted: () => React.ReactElement = () => (
+    <div className="ide-updater-dialog--downloaded">
+      <div>
+        {nls.localize(
+          'arduino/ide-updater/versionDownloaded',
+          'Arduino IDE {0} has been downloaded.',
+          version
+        )}
+      </div>
+      <div>
+        {nls.localize(
+          'arduino/ide-updater/closeToInstallNotice',
+          'Close the software and install the update on your machine.'
+        )}
+      </div>
+      <div className="buttons-container">
+        {closeButton}
+        <button
+          onClick={onCloseAndInstall}
+          type="button"
+          className="theia-button close-and-install"
+        >
+          {nls.localize(
+            'arduino/ide-updater/closeAndInstallButton',
+            'Close and Install'
+          )}
+        </button>
+      </div>
+    </div>
+  );
+
+  const DownloadStarted: () => React.ReactElement = () => (
+    <div className="ide-updater-dialog--downloading">
+      <div>
+        {nls.localize(
+          'arduino/ide-updater/downloadingNotice',
+          'Downloading the latest version of the Arduino IDE.'
+        )}
+      </div>
+      <ProgressBar percent={progress?.percent} showPercentage />
+    </div>
+  );
+
+  const PreDownload: () => React.ReactElement = () => (
+    <div className="ide-updater-dialog--pre-download">
+      <div className="ide-updater-dialog--logo-container">
+        <div className="ide-updater-dialog--logo"></div>
+      </div>
+      <div className="ide-updater-dialog--new-version-text dialogSection">
+        <div className="dialogRow">
+          <div className="bold">
+            {nls.localize(
+              'arduino/ide-updater/updateAvailable',
+              'Update Available'
+            )}
+          </div>
+        </div>
+        <div className="dialogRow">
+          {nls.localize(
+            'arduino/ide-updater/newVersionAvailable',
+            'A new version of Arduino IDE ({0}) is available for download.',
+            version
+          )}
+        </div>
+        {releaseNotes && (
+          <div className="dialogRow">
+            <div className="changelog-container" ref={changelogDivRef} />
+          </div>
+        )}
+        <div className="buttons-container">
+          <button
+            onClick={onSkipVersion}
+            type="button"
+            className="theia-button secondary skip-version"
+          >
+            {nls.localize(
+              'arduino/ide-updater/skipVersionButton',
+              'Skip Version'
+            )}
+          </button>
+          <div className="push"></div>
+          {closeButton}
+          <button
+            onClick={onDownload}
+            type="button"
+            className="theia-button primary"
+          >
+            {nls.localize('arduino/ide-updater/downloadButton', 'Download')}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+
+  const onGoToDownloadClick = (
+    event: React.SyntheticEvent<HTMLAnchorElement, Event>
+  ) => {
+    const { target } = event.nativeEvent;
+    if (target instanceof HTMLAnchorElement) {
+      event.nativeEvent.preventDefault();
+      windowService.openNewWindow(target.href, { external: true });
+      onClose();
+    }
+  };
+
+  const GoToDownloadPage: () => React.ReactElement = () => (
+    <div className="ide-updater-dialog--go-to-download-page">
+      <div>
+        {nls.localize(
+          'arduino/ide-updater/goToDownloadPage',
+          "An update for the Arduino IDE is available, but we're not able to download and install it automatically. Please go to the download page and download the latest version from there."
+        )}
+      </div>
+      <div className="buttons-container">
+        {closeButton}
+        <a
+          className="theia-button primary"
+          href="https://www.arduino.cc/en/software#experimental-software"
+          onClick={onGoToDownloadClick}
+        >
+          {nls.localize(
+            'arduino/ide-updater/goToDownloadButton',
+            'Go To Download'
+          )}
+        </a>
+      </div>
+    </div>
+  );
+
   return (
     <div className="ide-updater-dialog--content">
-      {downloadFinished ? (
-        <div className="ide-updater-dialog--downloaded">
-          <div>
-            {nls.localize(
-              'arduino/ide-updater/versionDownloaded',
-              'Arduino IDE {0} has been downloaded.',
-              version
-            )}
-          </div>
-          <div>
-            {nls.localize(
-              'arduino/ide-updater/closeToInstallNotice',
-              'Close the software and install the update on your machine.'
-            )}
-          </div>
-          <div className="buttons-container">
-            {closeButton}
-            <button
-              onClick={onCloseAndInstall}
-              type="button"
-              className="theia-button close-and-install"
-            >
-              {nls.localize(
-                'arduino/ide-updater/closeAndInstallButton',
-                'Close and Install'
-              )}
-            </button>
-          </div>
-        </div>
+      {!!error ? (
+        <GoToDownloadPage />
+      ) : downloadFinished ? (
+        <DownloadCompleted />
       ) : downloadStarted ? (
-        <div className="ide-updater-dialog--downloading">
-          <div>
-            {nls.localize(
-              'arduino/ide-updater/downloadingNotice',
-              'Downloading the latest version of the Arduino IDE.'
-            )}
-          </div>
-          <ProgressBar percent={progress?.percent} showPercentage />
-        </div>
+        <DownloadStarted />
       ) : (
-        <div className="ide-updater-dialog--pre-download">
-          <div className="ide-updater-dialog--logo-container">
-            <div className="ide-updater-dialog--logo"></div>
-          </div>
-          <div className="ide-updater-dialog--new-version-text dialogSection">
-            <div className="dialogRow">
-              <div className="bold">
-                {nls.localize(
-                  'arduino/ide-updater/updateAvailable',
-                  'Update Available'
-                )}
-              </div>
-            </div>
-            <div className="dialogRow">
-              {nls.localize(
-                'arduino/ide-updater/newVersionAvailable',
-                'A new version of Arduino IDE ({0}) is available for download.',
-                version
-              )}
-            </div>
-            {releaseNotes && (
-              <div className="dialogRow">
-                <div className="changelog-container" ref={changelogDivRef} />
-              </div>
-            )}
-            <div className="buttons-container">
-              <button
-                onClick={onSkipVersion}
-                type="button"
-                className="theia-button secondary skip-version"
-              >
-                {nls.localize(
-                  'arduino/ide-updater/skipVersionButton',
-                  'Skip Version'
-                )}
-              </button>
-              <div className="push"></div>
-              {closeButton}
-              <button
-                onClick={onDownload}
-                type="button"
-                className="theia-button primary"
-              >
-                {nls.localize('arduino/ide-updater/downloadButton', 'Download')}
-              </button>
-            </div>
-          </div>
-        </div>
+        <PreDownload />
       )}
-      {!!error && <div className="error-container">{error}</div>}
+      {/* {!!error && <div className="error-container">{error}</div>} */}
     </div>
   );
 };
