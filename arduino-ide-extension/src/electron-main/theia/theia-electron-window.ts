785d261bd4a2cc838478123122dd2cc38ee246b1
Akos Kitta <a.kitta@arduino.cc>
2022-05-31 16:11:20 +0200

    Aligned the stop handler code to Theia.
    Signed-off-by: Akos Kitta <a.kitta@arduino.cc>

--- arduino-ide-extension/src/electron-main/theia/theia-electron-window.ts
+++ arduino-ide-extension/src/electron-main/theia/theia-electron-window.ts
@@ -1,8 +1,8 @@
 import { injectable } from '@theia/core/shared/inversify';
-import { isWindows } from '@theia/core/lib/common/os';
 import { StopReason } from '@theia/core/lib/electron-common/messaging/electron-messages';
 import { TheiaElectronWindow as DefaultTheiaElectronWindow } from '@theia/core/lib/electron-main/theia-electron-window';
 import { FileUri } from '@theia/core/lib/node';
+import URI from '@theia/core/lib/common/uri';
 
 @injectable()
 export class TheiaElectronWindow extends DefaultTheiaElectronWindow {
@@ -10,16 +10,17 @@ export class TheiaElectronWindow extends DefaultTheiaElectronWindow {
     onSafeCallback: () => unknown,
     reason: StopReason
   ): Promise<boolean> {
-    // Only confirm close to windows that have loaded our front end.
-    let currentUrl = this.window.webContents.getURL(); // this comes from electron, expected to be an URL encoded string. e.g: space will be `%20`
-    let frontendUri = FileUri.create(
+    // Only confirm close to windows that have loaded our frontend.
+    // Both the windows's URL and the FS path of the `index.html` should be converted to the "same" format to be able to compare them. (#11226)
+    // Notes:
+    //  - Windows: file:///C:/path/to/somewhere vs file:///c%3A/path/to/somewhere
+    //  - macOS: file:///Applications/App%20Name.app/Contents vs /Applications/App Name.app/Contents
+    // This URL string comes from electron, we can expect that this is properly encoded URL. For example, a space is `%20`
+    const currentUrl = new URI(this.window.webContents.getURL()).toString();
+    // THEIA_FRONTEND_HTML_PATH is an FS path, we have to covert to an encoded URI string.
+    const frontendUri = FileUri.create(
       this.globals.THEIA_FRONTEND_HTML_PATH
-    ).toString(false); // Map the FS path to an URI, ensure the encoding is not skipped, so that a space will be `%20`.
-    // Since our resolved frontend HTML path might contain backward slashes on Windows, we normalize everything first.
-    if (isWindows) {
-      currentUrl = currentUrl.replace(/\\/g, '/');
-      frontendUri = frontendUri.replace(/\\/g, '/');
-    }
+    ).toString();
     const safeToClose =
       !currentUrl.includes(frontendUri) || (await this.checkSafeToStop(reason));
     if (safeToClose) {
