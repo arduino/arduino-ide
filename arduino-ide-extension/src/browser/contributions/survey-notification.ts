fb7ec54d60e6af7ab4eca0008862355e5f81cf81
Francesco Spissu <f.spissu@arduino.cc>
2022-06-14 15:59:31 +0200

    code clean up

--- arduino-ide-extension/src/browser/contributions/survey-notification.ts
+++ arduino-ide-extension/src/browser/contributions/survey-notification.ts
@@ -6,18 +6,13 @@ import { nls } from '@theia/core/lib/common';
 import { WindowService } from '@theia/core/lib/browser/window/window-service';
 import { ArduinoPreferences } from '../arduino-preferences';
 
-export type Survey = {
-  url: URL;
-  id: string;
-};
-
 const SURVEY_MESSAGE = nls.localize(
   'arduino/survey/surveyMessage',
   'Please help us improve by answering this super short survey. We value our community and would like to get to know our supporters a little better.'
 );
 const DO_NOT_SHOW_AGAIN = nls.localize(
   'arduino/survey/dismissSurvey',
-  'DON’T SHOW ANYMORE'
+  "DON'T SHOW ANYMORE"
 );
 const GO_TO_SURVEY = nls.localize(
   'arduino/survey/answerSurvey',
@@ -30,45 +25,53 @@ const surveyId = '17887b40-e1f0-4bd6-b9f0-a37f229ccd8b';
 @injectable()
 export class SurveyNotification implements FrontendApplicationContribution {
   @inject(MessageService)
-  protected readonly messageService: MessageService;
+  private readonly messageService: MessageService;
 
   @inject(LocalStorageService)
-  protected readonly localStorageService: LocalStorageService;
+  private readonly localStorageService: LocalStorageService;
 
   @inject(WindowService)
-  protected readonly windowService: WindowService;
+  private readonly windowService: WindowService;
 
   @inject(ArduinoPreferences)
-  protected readonly arduinoPreferences: ArduinoPreferences;
+  private readonly arduinoPreferences: ArduinoPreferences;
 
-  async onStart(): Promise<void> {
-    if (this.arduinoPreferences.get('arduino.survey.notification')) {
-      this.localStorageService
-        .getData(this.surveyKey(surveyId), undefined)
-        .then((surveyAnswered) => {
-          if (surveyAnswered !== undefined) {
-            return;
-          }
-          return this.messageService.info(
-            SURVEY_MESSAGE,
-            DO_NOT_SHOW_AGAIN,
-            GO_TO_SURVEY
-          );
-        })
-        .then((answer) => {
-          switch (answer) {
-            case GO_TO_SURVEY:
-              this.windowService.openNewWindow(SURVEY_BASE_URL + surveyId, {
-                external: true,
-              });
-              this.localStorageService.setData(this.surveyKey(surveyId), true);
-              break;
-            case DO_NOT_SHOW_AGAIN:
-              this.localStorageService.setData(this.surveyKey(surveyId), false);
-              break;
-          }
-        });
-    }
+  onStart(): void {
+    this.arduinoPreferences.ready.then(() => {
+      if (this.arduinoPreferences.get('arduino.survey.notification')) {
+        this.localStorageService
+          .getData(this.surveyKey(surveyId))
+          .then((surveyAnswered) => {
+            if (surveyAnswered !== undefined) {
+              return;
+            }
+            return this.messageService.info(
+              SURVEY_MESSAGE,
+              DO_NOT_SHOW_AGAIN,
+              GO_TO_SURVEY
+            );
+          })
+          .then((answer) => {
+            switch (answer) {
+              case GO_TO_SURVEY:
+                this.windowService.openNewWindow(SURVEY_BASE_URL + surveyId, {
+                  external: true,
+                });
+                this.localStorageService.setData(
+                  this.surveyKey(surveyId),
+                  true
+                );
+                break;
+              case DO_NOT_SHOW_AGAIN:
+                this.localStorageService.setData(
+                  this.surveyKey(surveyId),
+                  false
+                );
+                break;
+            }
+          });
+      }
+    });
   }
 
   private surveyKey(id: string): string {
