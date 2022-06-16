77698f8f8b206ac2af277f6b935938e79958dfaf
Francesco Spissu <f.spissu@arduino.cc>
2022-06-16 18:28:56 +0200

    document the purpose of the service

--- arduino-ide-extension/src/node/survey-service-impl.ts
+++ arduino-ide-extension/src/node/survey-service-impl.ts
@@ -1,15 +1,20 @@
 import { injectable } from '@theia/core/shared/inversify';
 import { SurveyNotificationService } from '../common/protocol/survey-service';
 
+/**
+ * Service for checking if it is the first instance of the IDE, in this case it sets a flag to true.
+ * This flag is used to prevent the survey notification from being visible in every open window. It must only be shown on one window.
+ */
 @injectable()
 export class SurveyNotificationServiceImpl
   implements SurveyNotificationService
 {
-  private isSurveyShown = false;
+  private surveyDidShow = false;
   async isFirstInstance(): Promise<boolean> {
-    if (this.isSurveyShown) {
+    if (this.surveyDidShow) {
       return false;
     }
-    return (this.isSurveyShown = true);
+    this.surveyDidShow = true;
+    return this.surveyDidShow;
   }
 }
