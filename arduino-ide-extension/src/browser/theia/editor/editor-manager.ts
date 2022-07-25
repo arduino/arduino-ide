0b361d424a976890a216d6c6ea2540e49a5de216
Akos Kitta <a.kitta@arduino.cc>
2022-07-25 18:24:29 +0200

    Aligned the editor widget factory API with Theia.
    To correctly use and parse the widget `id`.
    
    Ref: https://github.com/eclipse-theia/theia/commit/86a4fc66c112310fb39f50024ea4a2607ed5927e#r79349262
    Signed-off-by: Akos Kitta <a.kitta@arduino.cc>

--- arduino-ide-extension/src/browser/theia/editor/editor-manager.ts
+++ arduino-ide-extension/src/browser/theia/editor/editor-manager.ts
@@ -1,22 +1,9 @@
 import { injectable } from '@theia/core/shared/inversify';
-import { EditorWidget } from '@theia/editor/lib/browser';
-import {
-  EditorManager as TheiaEditorManager,
-  WidgetId,
-} from '@theia/editor/lib/browser/editor-manager';
+import { EditorManager as TheiaEditorManager } from '@theia/editor/lib/browser/editor-manager';
 
 @injectable()
 export class EditorManager extends TheiaEditorManager {
   protected override getOrCreateCounterForUri(): number {
     return 0;
-  }
-
-  protected override extractIdFromWidget(widget: EditorWidget): WidgetId {
-    const { id, uri } = super.extractIdFromWidget(widget);
-    // https://github.com/eclipse-theia/theia/commit/86a4fc66c112310fb39f50024ea4a2607ed5927e#r79349262
-    if (Number.isNaN(id)) {
-      return { uri, id: 0 };
-    }
-    return { id, uri };
   }
 }
--- arduino-ide-extension/src/browser/theia/editor/editor-widget-factory.ts
+++ arduino-ide-extension/src/browser/theia/editor/editor-widget-factory.ts
@@ -1,7 +1,10 @@
 import { inject, injectable } from '@theia/core/shared/inversify';
 import URI from '@theia/core/lib/common/uri';
 import { EditorWidget } from '@theia/editor/lib/browser';
-import { LabelProvider } from '@theia/core/lib/browser';
+import {
+  LabelProvider,
+  NavigatableWidgetOptions,
+} from '@theia/core/lib/browser';
 import { EditorWidgetFactory as TheiaEditorWidgetFactory } from '@theia/editor/lib/browser/editor-widget-factory';
 import {
   CurrentSketch,
@@ -21,8 +24,11 @@ export class EditorWidgetFactory extends TheiaEditorWidgetFactory {
   @inject(LabelProvider)
   protected override readonly labelProvider: LabelProvider;
 
-  protected override async createEditor(uri: URI): Promise<EditorWidget> {
-    const widget = await super.createEditor(uri);
+  protected override async createEditor(
+    uri: URI,
+    options?: NavigatableWidgetOptions
+  ): Promise<EditorWidget> {
+    const widget = await super.createEditor(uri, options);
     return this.maybeUpdateCaption(widget);
   }
 
