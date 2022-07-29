59b8a2d6bb6c7ea630d0ea65a1ea4aca10e093eb
Alberto Iannaccone <a.iannaccone@arduino.cc>
2022-07-29 15:09:53 +0200

    Register custom themes after the monaco theme init (#1257)
    Signed-off-by: Akos Kitta <a.kitta@arduino.cc>
    
    Co-authored-by: Akos Kitta <a.kitta@arduino.cc>

--- arduino-ide-extension/src/browser/utils/window.ts
+++ arduino-ide-extension/src/browser/utils/window.ts
@@ -5,3 +5,11 @@
 export function setURL(url: URL, data: any = {}): void {
   history.pushState(data, '', url);
 }
+
+/**
+ * If available from the `window` object, then it means, the IDE2 has successfully patched the `MonacoThemingService#init` static method,
+ * and can wait the custom theme registration.
+ */
+export const MonacoThemeServiceIsReady = Symbol(
+  '@arduino-ide#monaco-theme-service-is-ready'
+);
