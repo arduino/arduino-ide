f04306c8a5df813db5f6b1ac55017a494f8cd50d
Akos Kitta <a.kitta@arduino.cc>
2022-11-28 18:42:15 +0100

    fix: restored `setRepresentedFilename` on macOS
    Signed-off-by: Akos Kitta <a.kitta@arduino.cc>

--- arduino-ide-extension/src/browser/theia/core/window-title-updater.ts
+++ arduino-ide-extension/src/browser/theia/core/window-title-updater.ts
@@ -1,14 +1,17 @@
-import { NavigatableWidget } from '@theia/core/lib/browser';
+import * as remote from '@theia/core/electron-shared/@electron/remote';
 import { FrontendApplicationConfigProvider } from '@theia/core/lib/browser/frontend-application-config-provider';
+import { NavigatableWidget } from '@theia/core/lib/browser/navigatable-types';
 import { ApplicationShell } from '@theia/core/lib/browser/shell/application-shell';
 import { Widget } from '@theia/core/lib/browser/widgets/widget';
 import { WindowTitleUpdater as TheiaWindowTitleUpdater } from '@theia/core/lib/browser/window/window-title-updater';
 import { ApplicationServer } from '@theia/core/lib/common/application-protocol';
+import { isOSX } from '@theia/core/lib/common/os';
 import {
   inject,
   injectable,
   postConstruct,
 } from '@theia/core/shared/inversify';
+import { EditorWidget } from '@theia/editor/lib/browser/editor-widget';
 import { WorkspaceService } from '@theia/workspace/lib/browser/workspace-service';
 
 @injectable()
@@ -19,6 +22,8 @@ export class WindowTitleUpdater extends TheiaWindowTitleUpdater {
   private readonly applicationShell: ApplicationShell;
   @inject(WorkspaceService)
   private readonly workspaceService: WorkspaceService;
+
+  private _previousRepresentedFilename: string | undefined;
 
   private readonly applicationName =
     FrontendApplicationConfigProvider.get().applicationName;
@@ -39,7 +44,10 @@ export class WindowTitleUpdater extends TheiaWindowTitleUpdater {
   }
 
   protected override handleWidgetChange(widget?: Widget | undefined): void {
-    // Unlike Theia, IDE2 does not want to show in the window title if the current widget is dirty or now.
+    if (isOSX) {
+      this.maybeUpdateRepresentedFilename(widget);
+    }
+    // Unlike Theia, IDE2 does not want to show in the window title if the current widget is dirty or not.
     // Hence, IDE2 does not track widgets but updates the window title on current widget change.
     this.updateTitleWidget(widget);
   }
@@ -62,5 +70,18 @@ export class WindowTitleUpdater extends TheiaWindowTitleUpdater {
       }
     }
     this.windowTitleService.update({ rootName, appName, activeEditorShort });
+  }
+
+  private maybeUpdateRepresentedFilename(widget?: Widget | undefined): void {
+    if (widget instanceof EditorWidget) {
+      const { uri } = widget.editor;
+      const filename = uri.path.toString();
+      // Do not necessarily require the current window if not needed. It's a synchronous, blocking call.
+      if (this._previousRepresentedFilename !== filename) {
+        const currentWindow = remote.getCurrentWindow();
+        currentWindow.setRepresentedFilename(uri.path.toString());
+        this._previousRepresentedFilename = filename;
+      }
+    }
   }
 }
