blog2i2j/arduino.._..arduino-ide

fix: propagate monitor errors to the frontend
 - Handle when the board's platform is not installed (Closes #1974)
 - UX: Smoother monitor widget reset (Closes #1985)
 - Fixed monitor <input> readOnly state (Closes #1984)
 - Set monitor widget header color (Ref #682)

Closes #1508

Signed-off-by: Akos Kitta <a.kitta@arduino.cc>

arduino-ide-extension/src/common/protocol/monitor-service.ts
@@ -1,4 +1,4 @@
-import { Event, JsonRpcServer } from '@theia/core';
+import { ApplicationError, Event, JsonRpcServer, nls } from '@theia/core';
 import {
   PluggableMonitorSettings,
   MonitorSettings,
@@ -31,7 +31,7 @@ export interface MonitorManagerProxyClient {
   onMessagesReceived: Event<{ messages: string[] }>;
   onMonitorSettingsDidChange: Event<MonitorSettings>;
   onMonitorShouldReset: Event<void>;
-  connect(addressPort: number): void;
+  connect(addressPort: number): Promise<void>;
   disconnect(): void;
   getWebSocketPort(): number | undefined;
   isWSConnected(): Promise<boolean>;
@@ -46,7 +46,7 @@ export interface PluggableMonitorSetting {
   readonly id: string;
   // A human-readable label of the setting (to be displayed on the GUI)
   readonly label: string;
-  // The setting type (at the moment only "enum" is avaiable)
+  // The setting type (at the moment only "enum" is available)
   readonly type: string;
   // The values allowed on "enum" types
   readonly values: string[];
@@ -72,24 +72,170 @@ export namespace Monitor {
   };
 }
 
-export interface Status {}
-export type OK = Status;
-export interface ErrorStatus extends Status {
-  readonly message: string;
-}
-export namespace Status {
-  export function isOK(status: Status & { message?: string }): status is OK {
-    return !!status && typeof status.message !== 'string';
+export const MonitorErrorCodes = {
+  ConnectionFailed: 6001,
+  NotConnected: 6002,
+  AlreadyConnected: 6003,
+  MissingConfiguration: 6004,
+} as const;
+
+export const ConnectionFailedError = createMonitorError(
+  MonitorErrorCodes.ConnectionFailed
+);
+export const NotConnectedError = createMonitorError(
+  MonitorErrorCodes.NotConnected
+);
+export const AlreadyConnectedError = createMonitorError(
+  MonitorErrorCodes.AlreadyConnected
+);
+export const MissingConfigurationError = createMonitorError(
+  MonitorErrorCodes.MissingConfiguration
+);
+
+export function createConnectionFailedError(
+  port: Port,
+  details?: string
+): ApplicationError<number, PortDescriptor> {
+  const { protocol, address } = port;
+  let message;
+  if (details) {
+    const detailsWithPeriod = details.endsWith('.') ? details : `${details}.`;
+    message = nls.localize(
+      'arduino/monitor/connectionFailedErrorWithDetails',
+      '{0} Could not connect to {1} {2} port.',
+      detailsWithPeriod,
+      address,
+      protocol
+    );
+  } else {
+    message = nls.localize(
+      'arduino/monitor/connectionFailedError',
+      'Could not connect to {0} {1} port.',
+      address,
+      protocol
+    );
   }
-  export const OK: OK = {};
-  export const NOT_CONNECTED: ErrorStatus = { message: 'Not connected.' };
-  export const ALREADY_CONNECTED: ErrorStatus = {
-    message: 'Already connected.',
-  };
-  export const CONFIG_MISSING: ErrorStatus = {
-    message: 'Serial Config missing.',
-  };
-  export const UPLOAD_IN_PROGRESS: ErrorStatus = {
-    message: 'Upload in progress.',
-  };
+  return ConnectionFailedError(message, { protocol, address });
+}
+export function createNotConnectedError(
+  port: Port
+): ApplicationError<number, PortDescriptor> {
+  const { protocol, address } = port;
+  return NotConnectedError(
+    nls.localize(
+      'arduino/monitor/notConnectedError',
+      'Not connected to {0} {1} port.',
+      address,
+      protocol
+    ),
+    { protocol, address }
+  );
+}
+export function createAlreadyConnectedError(
+  port: Port
+): ApplicationError<number, PortDescriptor> {
+  const { protocol, address } = port;
+  return AlreadyConnectedError(
+    nls.localize(
+      'arduino/monitor/alreadyConnectedError',
+      'Could not connect to {0} {1} port. Already connected.',
+      address,
+      protocol
+    ),
+    { protocol, address }
+  );
+}
+export function createMissingConfigurationError(
+  port: Port
+): ApplicationError<number, PortDescriptor> {
+  const { protocol, address } = port;
+  return MissingConfigurationError(
+    nls.localize(
+      'arduino/monitor/missingConfigurationError',
+      'Could not connect to {0} {1} port. The monitor configuration is missing.',
+      address,
+      protocol
+    ),
+    { protocol, address }
+  );
+}
+
+/**
+ * Bare minimum representation of a port. Supports neither UI labels nor properties.
+ */
+interface PortDescriptor {
+  readonly protocol: string;
+  readonly address: string;
+}
+function createMonitorError(
+  code: number
+): ApplicationError.Constructor<number, PortDescriptor> {
+  return ApplicationError.declare(
+    code,
+    (message: string, data: PortDescriptor) => ({ data, message })
+  );
+}
+
+export interface MonitorConnectionError {
+  readonly errorMessage: string;
+}
+
+export type MonitorConnectionStatus =
+  | 'connecting'
+  | 'connected'
+  | 'not-connected'
+  | MonitorConnectionError;
+
+export function monitorConnectionStatusEquals(
+  left: MonitorConnectionStatus,
+  right: MonitorConnectionStatus
+): boolean {
+  if (typeof left === 'string') {
+    return typeof right === 'string' ? left === right : false;
+  }
+  return typeof right === 'object'
+    ? right.errorMessage === left.errorMessage
+    : false;
+}
+
+/**
+ * @deprecated see `MonitorState#connected`
+ */
+export function isMonitorConnected(
+  status: MonitorConnectionStatus
+): status is 'connected' {
+  return status === 'connected';
+}
+
+export function isMonitorConnectionError(
+  status: MonitorConnectionStatus
+): status is MonitorConnectionError {
+  return typeof status === 'object';
+}
+
+export interface MonitorState {
+  autoscroll: boolean;
+  timestamp: boolean;
+  lineEnding: MonitorEOL;
+  interpolate: boolean;
+  darkTheme: boolean;
+  wsPort: number;
+  serialPort: string;
+  connectionStatus: MonitorConnectionStatus;
+  /**
+   * @deprecated This property is never get by IDE2 only set. This value is present to be backward compatible with the plotter app.
+   * IDE2 uses `MonitorState#connectionStatus`.
+   */
+  connected: boolean;
+}
+export namespace MonitorState {
+  export interface Change<K extends keyof MonitorState> {
+    readonly property: K;
+    readonly value: MonitorState[K];
+  }
+}
+
+export type MonitorEOL = '' | '\n' | '\r' | '\r\n';
+export namespace MonitorEOL {
+  export const DEFAULT: MonitorEOL = '\n';
 }

arduino/arduino-ide

arduino-ide-extension/src/common/protocol/monitor-service.ts
@@ -1,4 +1,4 @@
-import { Event, JsonRpcServer } from '@theia/core';
+import { ApplicationError, Event, JsonRpcServer, nls } from '@theia/core';
 import {
   PluggableMonitorSettings,
   MonitorSettings,
@@ -31,7 +31,7 @@ export interface MonitorManagerProxyClient {
   onMessagesReceived: Event<{ messages: string[] }>;
   onMonitorSettingsDidChange: Event<MonitorSettings>;
   onMonitorShouldReset: Event<void>;
-  connect(addressPort: number): void;
+  connect(addressPort: number): Promise<void>;
   disconnect(): void;
   getWebSocketPort(): number | undefined;
   isWSConnected(): Promise<boolean>;
@@ -46,7 +46,7 @@ export interface PluggableMonitorSetting {
   readonly id: string;
   // A human-readable label of the setting (to be displayed on the GUI)
   readonly label: string;
-  // The setting type (at the moment only "enum" is avaiable)
+  // The setting type (at the moment only "enum" is available)
   readonly type: string;
   // The values allowed on "enum" types
   readonly values: string[];
@@ -72,24 +72,168 @@ export namespace Monitor {
   };
 }
 
-export interface Status {}
-export type OK = Status;
-export interface ErrorStatus extends Status {
-  readonly message: string;
-}
-export namespace Status {
-  export function isOK(status: Status & { message?: string }): status is OK {
-    return !!status && typeof status.message !== 'string';
+export const MonitorErrorCodes = {
+  ConnectionFailed: 6001,
+  NotConnected: 6002,
+  AlreadyConnected: 6003,
+  MissingConfiguration: 6004,
+} as const;
+
+export const ConnectionFailedError = declareMonitorError(
+  MonitorErrorCodes.ConnectionFailed
+);
+export const NotConnectedError = declareMonitorError(
+  MonitorErrorCodes.NotConnected
+);
+export const AlreadyConnectedError = declareMonitorError(
+  MonitorErrorCodes.AlreadyConnected
+);
+export const MissingConfigurationError = declareMonitorError(
+  MonitorErrorCodes.MissingConfiguration
+);
+
+export function createConnectionFailedError(
+  port: Port,
+  details?: string
+): ApplicationError<number, PortDescriptor> {
+  const { protocol, address } = port;
+  let message;
+  if (details) {
+    const detailsWithPeriod = details.endsWith('.') ? details : `${details}.`;
+    message = nls.localize(
+      'arduino/monitor/connectionFailedErrorWithDetails',
+      '{0} Could not connect to {1} {2} port.',
+      detailsWithPeriod,
+      address,
+      protocol
+    );
+  } else {
+    message = nls.localize(
+      'arduino/monitor/connectionFailedError',
+      'Could not connect to {0} {1} port.',
+      address,
+      protocol
+    );
   }
-  export const OK: OK = {};
-  export const NOT_CONNECTED: ErrorStatus = { message: 'Not connected.' };
-  export const ALREADY_CONNECTED: ErrorStatus = {
-    message: 'Already connected.',
-  };
-  export const CONFIG_MISSING: ErrorStatus = {
-    message: 'Serial Config missing.',
-  };
-  export const UPLOAD_IN_PROGRESS: ErrorStatus = {
-    message: 'Upload in progress.',
-  };
+  return ConnectionFailedError(message, { protocol, address });
+}
+export function createNotConnectedError(
+  port: Port
+): ApplicationError<number, PortDescriptor> {
+  const { protocol, address } = port;
+  return NotConnectedError(
+    nls.localize(
+      'arduino/monitor/notConnectedError',
+      'Not connected to {0} {1} port.',
+      address,
+      protocol
+    ),
+    { protocol, address }
+  );
+}
+export function createAlreadyConnectedError(
+  port: Port
+): ApplicationError<number, PortDescriptor> {
+  const { protocol, address } = port;
+  return AlreadyConnectedError(
+    nls.localize(
+      'arduino/monitor/alreadyConnectedError',
+      'Could not connect to {0} {1} port. Already connected.',
+      address,
+      protocol
+    ),
+    { protocol, address }
+  );
+}
+export function createMissingConfigurationError(
+  port: Port
+): ApplicationError<number, PortDescriptor> {
+  const { protocol, address } = port;
+  return MissingConfigurationError(
+    nls.localize(
+      'arduino/monitor/missingConfigurationError',
+      'Could not connect to {0} {1} port. The monitor configuration is missing.',
+      address,
+      protocol
+    ),
+    { protocol, address }
+  );
+}
+
+/**
+ * Bare minimum representation of a port. Supports neither UI labels nor properties.
+ */
+interface PortDescriptor {
+  readonly protocol: string;
+  readonly address: string;
+}
+function declareMonitorError(
+  code: number
+): ApplicationError.Constructor<number, PortDescriptor> {
+  return ApplicationError.declare(
+    code,
+    (message: string, data: PortDescriptor) => ({ data, message })
+  );
+}
+
+export interface MonitorConnectionError {
+  readonly errorMessage: string;
+}
+
+export type MonitorConnectionStatus =
+  | 'connecting'
+  | 'connected'
+  | 'not-connected'
+  | MonitorConnectionError;
+
+export function monitorConnectionStatusEquals(
+  left: MonitorConnectionStatus,
+  right: MonitorConnectionStatus
+): boolean {
+  if (typeof left === 'object' && typeof right === 'object') {
+    return left.errorMessage === right.errorMessage;
+  }
+  return left === right;
+}
+
+/**
+ * @deprecated see `MonitorState#connected`
+ */
+export function isMonitorConnected(
+  status: MonitorConnectionStatus
+): status is 'connected' {
+  return status === 'connected';
+}
+
+export function isMonitorConnectionError(
+  status: MonitorConnectionStatus
+): status is MonitorConnectionError {
+  return typeof status === 'object';
+}
+
+export interface MonitorState {
+  autoscroll: boolean;
+  timestamp: boolean;
+  lineEnding: MonitorEOL;
+  interpolate: boolean;
+  darkTheme: boolean;
+  wsPort: number;
+  serialPort: string;
+  connectionStatus: MonitorConnectionStatus;
+  /**
+   * @deprecated This property is never get by IDE2 only set. This value is present to be backward compatible with the plotter app.
+   * IDE2 uses `MonitorState#connectionStatus`.
+   */
+  connected: boolean;
+}
+export namespace MonitorState {
+  export interface Change<K extends keyof MonitorState> {
+    readonly property: K;
+    readonly value: MonitorState[K];
+  }
+}
+
+export type MonitorEOL = '' | '\n' | '\r' | '\r\n';
+export namespace MonitorEOL {
+  export const DEFAULT: MonitorEOL = '\n';
 }