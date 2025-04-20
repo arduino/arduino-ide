9fed4fbc48331a9e078ab1236159c09bd20cea87
neochaos42 <125705606+neochaos42@users.noreply.github.com>
2025-04-20 14:26:26 -0500

    Update post-package.js

--- electron-app/scripts/post-package.js
+++ electron-app/scripts/post-package.js
@@ -131,7 +131,7 @@ async function copyFilesToBuildArtifacts() {
     case 'linux': {
       filesToCopy.push(
         ...glob
-          .sync('**/arduino-ide*.{zip,AppImage}', { cwd })
+          .sync('**/arduino-ide*.{zip,AppImage,rpm}', { cwd })
           .map((p) => path.join(cwd, p))
       );
       break;
